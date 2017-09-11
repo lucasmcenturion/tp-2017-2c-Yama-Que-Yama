aacbcd79840ddaddf19a7f212917b68646d4e4d6
molinajulian <molina.bohemio@gmail.com>
2017-09-11 02:05:43 -0300

    Se establece conexion entre fs y yama. Proximamente dataNode a fs

--- filesystem.c
+++ filesystem.c
@@ -37,28 +37,38 @@ int main(){
 	struct sockaddr_in addr;  //  usamos sockaddr_in para manipular los elementos de la dirección del socket.
 	int addrlen= sizeof(addr);
 	struct addrinfo hints;
-	struct addrinfo *serverInfo;
-	int listenningSocket, rv;
+	struct addrinfo *server_info;
+	int listenning_socket, rv, socket_nueva_conexion;
   
 	memset(&hints, 0, sizeof(hints));  // Relleno con ceros el resto de la estructura. 
 	hints.ai_family = AF_INET;         // Seteo la familia de direcciones en inet. 
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;  // Usamos sockets de flujo, bidireccionales. 
 
-	if ((rv =getaddrinfo(NULL, PUERTO, &hints, &serverInfo)) != 0) {
+	if ((rv =getaddrinfo(NULL, PUERTO, &hints, &server_info)) != 0) {
 		printf("%i\n",rv );
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 	}
-	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);  // Obtengo el descriptor de fichero.
-	printf("%s%i\n","El valor del Listenning Socket es: ",listenningSocket );
+	listenning_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);  // Obtengo el descriptor de fichero.
+	printf("%s%i\n","El valor del Listenning Socket es: ",listenning_socket );
 	printf("%s \n", "Socket OK");
 
-	if(bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen)==-1) {  
+	if(bind(listenning_socket,server_info->ai_addr, server_info->ai_addrlen)==-1) {  
 		perror("Error en el bind."); exit(1);
 	}
+	int yes=1;
 	printf("%s \n", "Bind OK\n");
-	freeaddrinfo(serverInfo);
+	freeaddrinfo(server_info);
 
-	if( rv = listen( listenningSocket , BACKLOG )== -1) perror("Error en el listen");  // listen(descriptor de fichero, n conexiones permitidas).
+	if( rv = listen( listenning_socket , BACKLOG )== -1) perror("Error en el listen");  // listen(descriptor de fichero, n conexiones permitidas).
 	printf("%s \n", "El Servidor se encuentra OK para escuchar conexiones.");
+
+	char *un_buffer=malloc(sizeof(char));
+	if ((socket_nueva_conexion = accept(listenning_socket,(struct sockaddr *)&addr,&addrlen)) == -1){
+			perror("Error con conexion entrante");
+		
+	}else{
+		 recv(socket_nueva_conexion,un_buffer,sizeof(un_buffer),0);
+	}
+	printf("%s\n",un_buffer);
 }
--- yama.c
+++ yama.c
@@ -27,7 +27,7 @@
 #define BACKLOG 5  // Número de conexiones permitidas en la cola de entrada.
 
 int main(){
-	char *PUERTO,*PUERTO_FS,*RETARDO_PLANIFICACION,*FS_IP,*ALGORITMO_BALANCEO;
+	char *PUERTO,*FS_PUERTO,*RETARDO_PLANIFICACION,*FS_IP,*ALGORITMO_BALANCEO;
 	t_config *CFG;
 	CFG = config_create("yamaCFG.txt");   // Defino de dónde voy a leer la config.
 	PUERTO= config_get_string_value(CFG ,"PUERTO");  // Obtengo el valor de puerto.
@@ -43,29 +43,43 @@ int main(){
 	struct sockaddr_in addr;  //  usamos sockaddr_in para manipular los elementos de la dirección del socket.
 	int addrlen= sizeof(addr);
 	struct addrinfo hints;
-	struct addrinfo *serverInfo;
-	int listenningSocket, rv;
+	struct addrinfo *server_info;
+	int listenning_socket, rv, rv_fs,socket_fs;
   
 	memset(&hints, 0, sizeof(hints));  // Relleno con ceros el resto de la estructura. 
 	hints.ai_family = AF_INET;         // Seteo la familia de direcciones en inet. 
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;  // Usamos sockets de flujo, bidireccionales. 
 
-	if ((rv =getaddrinfo(NULL, PUERTO, &hints, &serverInfo)) != 0) {
+	if ((rv =getaddrinfo(NULL, PUERTO, &hints, &server_info)) != 0) {
 		printf("%i\n",rv );
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 	}
-	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);  // Obtengo el descriptor de fichero.
-	printf("%s%i\n","El valor del Listenning Socket es: ",listenningSocket );
+	listenning_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);  // Obtengo el descriptor de fichero.
+	printf("%s%i\n","El valor del Listenning Socket es: ",listenning_socket );
 	printf("%s \n", "Socket OK");
 
-	if(bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen)==-1) {  
+	if(bind(listenning_socket,server_info->ai_addr, server_info->ai_addrlen)==-1) {  
 		perror("Error en el bind."); exit(1);
 	}
 	printf("%s \n", "Bind OK\n");
-	freeaddrinfo(serverInfo);
 
-	if( rv = listen( listenningSocket , BACKLOG )== -1) perror("Error en el listen");  // listen(descriptor de fichero, n conexiones permitidas).
+	if( rv = listen( listenning_socket , BACKLOG )== -1) perror("Error en el listen");  // listen(descriptor de fichero, n conexiones permitidas).
 	printf("%s \n", "El Servidor se encuentra OK para escuchar conexiones.");
+
+	if ((rv_fs =getaddrinfo(FS_IP, FS_PUERTO, &hints, &server_info)) != 0) {
+		printf("%i\n",rv );
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+	}
+
+	socket_fs=socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+	printf("%s%i\n","El valor del socket memoria es: " , socket_fs);
+
+	if ((rv_fs=connect(socket_fs, server_info->ai_addr, server_info->ai_addrlen)) ==-1)
+	 {
+	 	perror("No se pudo conectar con fs");
+	 }else{
+	 	send(socket_fs,"1",sizeof(char),0);
+	 } 
 
 }
