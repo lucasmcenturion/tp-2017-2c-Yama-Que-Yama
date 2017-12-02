b394eb0182f1057bec0ba5c3429a0588b74ca3de
ivigilante <i.vigilante@me.com>
2017-12-02 15:58:51 -0300

    Update subdir.mk

--- nodo/datanode/Debug/src/subdir.mk
+++ nodo/datanode/Debug/src/subdir.mk
@@ -5,8 +5,8 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../src/datanode.c \
-/home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/helper.c \
-/home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/sockets.c 
+/home/utnso/tp-2017-2c-Yama-Que-Yama/Bibliotecas/helper.c \
+/home/utnso/tp-2017-2c-Yama-Que-Yama/Bibliotecas/sockets.c 
 
 OBJS += \
 ./src/datanode.o \
@@ -23,21 +23,21 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas -Ilcommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I/home/utnso/tp-2017-2c-Yama-Que-Yama/Bibliotecas -Ilcommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-src/helper.o: /home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/helper.c
+src/helper.o: /home/utnso/tp-2017-2c-Yama-Que-Yama/Bibliotecas/helper.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas -Ilcommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I/home/utnso/tp-2017-2c-Yama-Que-Yama/Bibliotecas -Ilcommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-src/sockets.o: /home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/sockets.c
+src/sockets.o: /home/utnso/tp-2017-2c-Yama-Que-Yama/Bibliotecas/sockets.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas -Ilcommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I/home/utnso/tp-2017-2c-Yama-Que-Yama/Bibliotecas -Ilcommons -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
