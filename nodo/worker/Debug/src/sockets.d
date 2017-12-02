c0cd58df880d0a21aae7921e6df32b31efba6b20
lucasmcenturion <lucasmcenturion@gmail.com>
2017-12-02 14:21:56 -0300

    se agregan las cosas

---
+++ nodo/worker/Debug/src/sockets.d
@@ -0,0 +1,8 @@
+src/sockets.d: \
+ /home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/sockets.c \
+ /home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/sockets.h \
+ /home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/helper.h
+
+/home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/sockets.h:
+
+/home/utnso/workspace/tp-2017-2c-Yama-Que-Yama/Bibliotecas/helper.h:
